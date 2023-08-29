c5f4ae9efdbcc22a608f0f307ba31278b2933ebf
chenruifeng <chenruifeng@ncti-gba.cn>
2023-08-29 14:20:30 +0800

    deploy to repo at 2023-08-29 14:20:30.005846

--- cae-demo/src/app/app-routing.module.ts
+++ cae-demo/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
-import {DockModule} from 'primeng/dock';
+import { DockModule } from 'primeng/dock';
 import { GeometryComponent } from './components/geometry/geometry.component';
 
 const routes: Routes = [
-  {title:'CAE-Demo',path:'',pathMatch:'full',redirectTo:'geometry'},
-  {title:'CAE-Geom',path:'geometry',pathMatch:'full',component:GeometryComponent},
+  { title: 'CAE-Demo', path: '', pathMatch: 'full', redirectTo: 'geometry' },
+  { title: 'CAE-Geom Viewer [OCCT]', path: 'geometry', pathMatch: 'full', component: GeometryComponent },
 ];
 
 @NgModule({
